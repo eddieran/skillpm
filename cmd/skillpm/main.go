a32be0269e2f214e0d96a220d85a892c463049b3
eddie <edding.suree@gmail.com>
2026-02-18 16:40:51 +0800

    feat: add eject alias for remove command

--- cmd/skillpm/main.go
+++ cmd/skillpm/main.go
@@ -339,7 +339,7 @@ func newRemoveCmd(newSvc func() (*app.Service, error), jsonOutput *bool) *cobra.
 	var agentName string
 	cmd := &cobra.Command{
 		Use:     "remove [source/skill ...]",
-		Aliases: []string{"detach"},
+		Aliases: []string{"detach", "eject"},
 		Short:   "Remove injected skills from target agent",
 		Args:    cobra.ArbitraryArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
